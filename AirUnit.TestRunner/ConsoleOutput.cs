04ffe5c06496ead837105a70458383810cb52e29
Daniel Harrison <daniel.harrison@checkout.com>
2019-03-21 00:12:04 +0400

    Add some colours

--- AirUnit.TestRunner/ConsoleOutput.cs
+++ AirUnit.TestRunner/ConsoleOutput.cs
@@ -6,13 +6,17 @@ namespace AirUnit.TestRunner
     {
         public void Pass(string fixtureName, string testName)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"PASS: {fixtureName}.{testName}");
+            Console.ResetColor();
         }
 
         public void Fail(string fixtureName, string testName, string exception)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"FAIL: {fixtureName}.{testName}");
             Console.WriteLine(exception);
+            Console.ResetColor();
         }
     }
 }
